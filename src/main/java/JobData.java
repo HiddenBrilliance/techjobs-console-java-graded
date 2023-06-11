9fd2b76c8939a824de6fb5a4592ea33837c3b159
Aaron Alexander <104107484+HiddenBrilliance@users.noreply.github.com>
2023-06-11 11:55:06 -0500

    worked on implementing findJobsbyValue

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -58,12 +58,12 @@ public class JobData {
     /**
      * Returns results of search the jobs data by key/value, using
      * inclusion of the search term.
-     *
+     * <p>
      * For example, searching for employer "Enterprise" will include results
      * with "Enterprise Holdings, Inc".
      *
-     * @param column   Column that should be searched.
-     * @param value Value of teh field to search for
+     * @param column Column that should be searched.
+     * @param value  Value of teh field to search for
      * @return List of all jobs matching the criteria
      */
     public static ArrayList<HashMap<String, String>> findByColumnAndValue(String column, String value) {
@@ -89,85 +89,70 @@ public class JobData {
      * Search all columns for the given term
      *
      * @param value The search term to look for
-     * @return      List of all jobs with at least one field containing the value
+     * @return
      */
     public static ArrayList<HashMap<String, String>> findByValue(String value) {
 
         // load data, if not already loaded
         loadData();
+        ArrayList<HashMap<String, String>> matchingJobs = new ArrayList<>(); // new array list
 
         for (HashMap<String, String> job : allJobs) { // outer for each job in allJobs
-            Set<Map.Entry<String, String>> entries = job.entrySet(); // job info for each job turned into a set
+            Set<Map.Entry<String, String>> jobEntries = job.entrySet(); // job info for each job turned into a set
 
-            for (Map.Entry<String, String> entry : entries){// iterates for each entry inm
-            String jobMatch = entry.getValue(); // string for value of entry
-            if(value.contains(jobMatch)){ // conditional if value contains jobMatch
-                System.out.println(value); //print value
-            }
-            else if (!value.contains(jobMatch)) { // conditional if it does not contain value
-                System.out.println("No results");
-            }
-            }
+            for (Map.Entry<String, String> entry : jobEntries) {// iterates for each entry inm
+                String JobEntryValue = entry.getValue(); // string for value of entry
 
-        }
+                if (JobEntryValue.toLowerCase().contains(value.toLowerCase())) { // conditional if value contains jobMatch
+                  matchingJobs.add(job);// adds the job with matching value
 
-
-//       ArrayList<HashMap<String, String>>valueList = new ArrayList<>();
-//
-//        for (int i = 0; i < valueList.size() ; i++) {
-//
-//            //outer loop access the value
-//        for (HashMap<String, String> searchValue : findByValue(value)) {
-//           //conditional if statement
-//            if(searchValue.containsValue(value)){
-//               System.out.println("it prints value");
-//            }
-//        }
-//        }
-
-        // TODO - implement this method
-        return findByValue(value);
-    }
-
-    /**
-     * Read in data from a CSV file and store it in a list
-     */
-    private static void loadData() {
-
-        // Only load data once
-        if (isDataLoaded) {
-            return;
-        }
-
-        try {
-
-            // Open the CSV file and set up pull out column header info and records
-            Reader in = new FileReader(DATA_FILE);
-            CSVParser parser = CSVFormat.RFC4180.withFirstRecordAsHeader().parse(in);
-            List<CSVRecord> records = parser.getRecords();
-            Integer numberOfColumns = records.get(0).size();
-            String[] headers = parser.getHeaderMap().keySet().toArray(new String[numberOfColumns]);
-
-            allJobs = new ArrayList<>();
-
-            // Put the records into a more friendly format
-            for (CSVRecord record : records) {
-                HashMap<String, String> newJob = new HashMap<>();
-
-                for (String headerLabel : headers) {
-                    newJob.put(headerLabel, record.get(headerLabel));
                 }
 
-                allJobs.add(newJob);
+                // TODO - implement this method
+                return matchingJobs;
             }
 
-            // flag the data as loaded, so we don't do it twice
-            isDataLoaded = true;
+            /**
+             * Read in data from a CSV file and store it in a list
+             */
+            private static void loadData() {
 
-        } catch (IOException e) {
-            System.out.println("Failed to load job data");
-            e.printStackTrace();
+                // Only load data once
+                if (isDataLoaded) {
+                    return;
+                }
+
+                try {
+
+                    // Open the CSV file and set up pull out column header info and records
+                    Reader in = new FileReader(DATA_FILE);
+                    CSVParser parser = CSVFormat.RFC4180.withFirstRecordAsHeader().parse(in);
+                    List<CSVRecord> records = parser.getRecords();
+                    Integer numberOfColumns = records.get(0).size();
+                    String[] headers = parser.getHeaderMap().keySet().toArray(new String[numberOfColumns]);
+
+                    allJobs = new ArrayList<>();
+
+                    // Put the records into a more friendly format
+                    for (CSVRecord record : records) {
+                        HashMap<String, String> newJob = new HashMap<>();
+
+                        for (String headerLabel : headers) {
+                            newJob.put(headerLabel, record.get(headerLabel));
+                        }
+
+                        allJobs.add(newJob);
+                    }
+
+                    // flag the data as loaded, so we don't do it twice
+                    isDataLoaded = true;
+
+                } catch (IOException e) {
+                    System.out.println("Failed to load job data");
+                    e.printStackTrace();
+                }
+            }
+
         }
     }
-
 }
